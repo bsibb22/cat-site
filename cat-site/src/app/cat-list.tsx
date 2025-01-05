37200e87f27014a17a3d2945578a1fbb0f8b5232
Brandon S <116681127+bsibb22@users.noreply.github.com>
2025-01-05 17:52:13 -0500

    Added basic styling to cat display page

--- cat-site/src/app/cat-list.tsx
+++ cat-site/src/app/cat-list.tsx
@@ -4,13 +4,13 @@ import Link from "next/link"
 
 export default function CatsList({ cats }: { cats: Cat[] }) {
     return (
-        <div className="mx-auto">
-            <div className="grid grid-cols-3 justify-center gap-8">
+        <div className="container justify-center mx-auto">
+            <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
                 {cats.map(cat => (
                     <div key={cat.id}>
-                        <Image src={cat.imageUrl} alt={cat.name} width={150} height={150}></Image>
-                        <h2>{cat.name}</h2>
-                        <p>${cat.price}</p>
+                        <Image className="mx-auto rounded-lg hover:shadow-lg" src={cat.imageUrl} alt={cat.name} width={300} height={300}></Image>
+                        <h2 className="text-center font-black text-slate-900 font-Lexend">{cat.name.toLowerCase()}</h2>
+                        <p className="text-center text-slate-900 font-Lexend">${cat.price}</p>
                     </div>
                 ))}
             </div>
